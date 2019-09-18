a5e7d90ed63adbd035272c066b0a6d39b7e5888a
Michelle Lam <shelly2.lam@gmail.com>
2019-09-18 16:03:06 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LoginContainer from './containers/LoginContainer'
+
+function App() {
+  return (
+    <div className="App">
+      <LoginContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/LoginContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import {Button} from 'semantic-ui-react'
+
+class LoginContainer extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <a href='http://localhost:3000/login'>Login Here</a>
+            </div>
+        )
+    }
+}
